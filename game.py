7bcf6a14a83dcf29392476db29ccdf1c3195c488
ChrUbuntu User <user@chromebox-003.(none)>
2013-06-26 16:21:21 -0700

    tues commit

--- game.py
+++ game.py
@@ -19,8 +19,58 @@ class Rock(GameElement):
     IMAGE = "Rock"
     SOLID = True
 
-# should this be here?
-rock = Rock()
+class DoorKey(GameElement):
+    IMAGE = "Key"
+    SOLID = True
+
+    def interact(instance, character):
+        print type(instance)
+        # if player has special gem, can get key
+        # if player does not have special gem, gets error message to get it
+        specialgem = SpecialGem()
+        # using same convention as has_key because cannot have else in for loop
+        has_specialgem = False
+        for item in character.inventory:
+            if type(specialgem) == type(item):
+                has_specialgem = True
+                character.inventory.append(instance) # instance is adding door key
+                instance.SOLID = False
+                GAME_BOARD.draw_msg("You just acquired the key!")
+        if not has_specialgem: 
+            GAME_BOARD.draw_msg("You need the Orange Gem to get the key!")
+
+class OpenDoor(GameElement):
+    IMAGE = "DoorOpen"
+    SOLID = False
+
+class ClosedDoor(GameElement):
+    IMAGE = "DoorClosed"
+    SOLID = True
+    def interact(instance, character):
+        #this is triggered when you run into the door
+        #player is lowercase b/c is passed in as lowercase
+        #interact function is when things happen most of the time in this game
+        opendoor = OpenDoor()
+        #need to register opendoor on gameboard
+        GAME_BOARD.register(opendoor)
+        doorkey = DoorKey()
+        #opendoor = getattr(opendoor, OpenDoor())
+        # if doorkey is in player inventory, change closeddoor to open door when player enters square
+        print doorkey
+        print character.inventory
+        print type(doorkey)
+        has_key = False
+        for item in character.inventory:
+            if type(doorkey) == type(item):
+                #turn closed door into open door 
+                has_key =True
+                GAME_BOARD.del_el(instance.x, instance.y)
+                GAME_BOARD.draw_msg("The key opened the door!")
+                #delete instance
+                GAME_BOARD.set_el(instance.x, instance.y, opendoor)
+        # else, show message below
+        if not has_key:
+            GAME_BOARD.draw_msg("You need the key to open this door!")
 
 class Character(GameElement):
     #IMAGE = "Girl"
@@ -42,33 +92,32 @@ class Character(GameElement):
             return (self.x+1, self.y)
         return None
 
+class TallTree(Character):
+    IMAGE = "TallTree"
+    SOLID = True
+
+    def __init__(self):
+        GameElement.__init__(self)
+        self.inventory = []
+
+    # don't need to override next_pos cause only PLAYER is moving (see below)
+
 class Gem(GameElement):
     IMAGE = "BlueGem"
     def interact(self, player):
-        player.inventory.append(self)
+        player.inventory.append(self) # when player runs into gem it's added to player stash
         GAME_BOARD.draw_msg("You just acquired a gem! You have %d items!"%(len(player.inventory)))
 
+class SpecialGem(GameElement):
+    IMAGE = "OrangeGem"
+    def interact(self, player):
+        player.inventory.append(self)
+        GAME_BOARD.draw_msg("You got the special gem! You have %d items!" % (len(player.inventory)))
 
-pass
 ####   End class definitions    ####
 
 def initialize():
     """Put game initialization code here"""
-
-    # Initialize and register rock 1
-    #rock1 = Rock()
-    #GAME_BOARD.register(rock1)
-    #GAME_BOARD.set_el(1, 1, rock1)
-
-    # Initialize and register rock 2
-    #rock2 = Rock()
-    #GAME_BOARD.register(rock2)
-    #GAME_BOARD.set_el(2, 2, rock2)
-
-    #initialize and register rock 3
-    #rock3 = Rock()
-    #GAME_BOARD.register(rock3)
-    #GAME_BOARD.set_el(5,2, rock3)
 
     rock_positions = [(2,1),(1,2),(3,2),(2,3)]
 
@@ -93,10 +142,6 @@ def initialize():
     GAME_BOARD.set_el(2, 2, PLAYER)
     print PLAYER
 
- 
-
-    pass
-
     for rock in rocks:
         print rock
 
@@ -106,6 +151,22 @@ def initialize():
     gem = Gem()
     GAME_BOARD.register(gem)
     GAME_BOARD.set_el(3, 1, gem)
+
+    specialgem = SpecialGem()
+    GAME_BOARD.register(specialgem)
+    GAME_BOARD.set_el(4, 4, specialgem)
+
+    closedDoor = ClosedDoor()
+    GAME_BOARD.register(closedDoor)
+    GAME_BOARD.set_el(0,2, closedDoor)
+
+    doorkey = DoorKey()
+    GAME_BOARD.register(doorkey)
+    GAME_BOARD.set_el(0,4, doorkey)
+
+    talltree = TallTree()
+    GAME_BOARD.register(talltree)
+    GAME_BOARD.set_el(0,3, talltree)
 
 def keyboard_handler():
     direction = None
@@ -130,9 +191,6 @@ def keyboard_handler():
         #next x coordinate is the first item in next_location
         next_y = next_location[1]
         #next y coordinate is the second item in next_location
-        
-
-        #testing deletion
 
         if next_x >= GAME_WIDTH or next_x<0:
             GAME_BOARD.draw_msg("Das is verboten")
@@ -142,8 +200,6 @@ def keyboard_handler():
             GAME_BOARD.draw_msg("Das is verboten")            
             return
             #existing_el = GAME_BOARD.get_el(next_x, next_y)
-
-        #########
         
         existing_el = GAME_BOARD.get_el(next_x, next_y)
         if existing_el:
@@ -152,21 +208,7 @@ def keyboard_handler():
         if existing_el is None or not existing_el.SOLID:
             GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
             GAME_BOARD.set_el(next_x, next_y, PLAYER)
-        #     except 
-        #         if next_x >= GAME_WIDTH or next_x < 0
-        #     except 
-        #         if next_y >= GAME_HEIGHT or next_y < 0
-        # #if next position is out of range of board
-        #game board = game board
 
-
-
-
-        #GAME_BOARD.del_el(PLAYER.x, PLAYER.y)
-        #deleting player from old posn
-
-        #GAME_BOARD.set_el(next_x, next_y, PLAYER)
-        #resetting player to next/new position
 
 
 
